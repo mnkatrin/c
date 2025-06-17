954875105b40bb672c98fdb042fdb2e79c3fe058
mnkatrin <mankovskaiaekaterinav@gmail.com>
2025-06-18 01:46:24 +0300

    Задание 2

---
+++ FIle/file2.c
@@ -0,0 +1,21 @@
+//Маньковская Е.В. 313ИС-22
+/*Каждую строку во входном файле сохранить в выходной файл в
+обратном порядке*/
+#include <stdio.h>
+#define N 200
+int main() {
+	FILE *input = fopen("hello.txt","r");
+	FILE *output = fopen("output.txt","w");
+	char arr[N];
+	while (fgets(arr,N,input)) {
+		int i = 0;
+		for (; arr[i]!='\0' && arr[i] != '\n'; i++) {}
+		while (i > 0) {
+			i--;
+			fputc(arr[i], output);
+		}
+		fputc('\n', output);
+	}
+	fclose(input);
+	fclose(output);
+}
