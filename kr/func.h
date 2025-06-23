817a81110f96343d6034b5af73e4dc9928565398
mnkatrin <mankovskaiaekaterinav@gmail.com>
2025-06-23 23:26:06 +0300

    func.h

--- kr/func.h
+++ kr/func.h
@@ -7,6 +7,7 @@ int load(char *file_name, Information info[], int limit);
 void rec_to_s(char s[], Information info);
 void echo_print(Information info[], int limit);
 void number_of_entries(Information info[], int limit,char *file_name);
+void fioname();
 
 
 #endif 
