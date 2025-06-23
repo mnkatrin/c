c82a549d4c28b6f1075235de0d4bdf28941dacd8
mnkatrin <mankovskaiaekaterinav@gmail.com>
2025-06-23 16:42:45 +0300

    Add files via upload

--- kr/info.h
+++ kr/info.h
@@ -1,12 +1,9 @@
-#ifndef FUNC_H_INCLUDED
-#define FUNC_H_INCLUDED
-#include "info.h"
-
-void show_menu();
-int load(char *file_name, Information info[], int limit);
-void rec_to_s(char s[], Information info);
-void echo_print(Information info[], int limit);
-void number_of_entries(Information info[], int limit,char *file_name);
-
-
-#endif 
+#ifndef INFO_H_INCLUDED
+#define INFO_H_INCLUDED
+typedef struct _info{
+	char specification[50];
+	char name[50];
+	char designation[10];
+	int position;
+} Information;
+#endif
